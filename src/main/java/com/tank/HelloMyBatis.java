ed63e8581a5124a86a90678e7b4346235294e2c5
fuchun198435163.com <tank198435@163.com>
2019-04-07 20:32:38 +0800

    [fuchun] #Tech support redis standalone connection

--- src/main/java/com/tank/HelloMyBatis.java
+++ src/main/java/com/tank/HelloMyBatis.java
@@ -1,9 +1,7 @@
 package com.tank;
 
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
---
+++ src/main/java/com/tank/config/RedisConf.java
@@ -0,0 +1,46 @@
+package com.tank.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author fuchun
+ */
+@Configuration
+public class RedisConf {
+
+  @Bean
+  public JedisConnectionFactory initJedisConnectionFactory() {
+    final RedisStandaloneConfiguration conf = this.initStandaloneRedisConfig();
+    final JedisConnectionFactory factory = new JedisConnectionFactory(conf);
+    return factory;
+  }
+
+
+  @Bean
+  public RedisTemplate<String, String> initRedisTemplate(@Autowired JedisConnectionFactory jedisConnectionFactory) {
+    final RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(jedisConnectionFactory);
+    return redisTemplate;
+  }
+
+  private RedisStandaloneConfiguration initStandaloneRedisConfig() {
+    final RedisStandaloneConfiguration conf = new RedisStandaloneConfiguration();
+    String url = env.getProperty("spring.redis.url");
+    Integer port = Integer.parseInt(env.getProperty("spring.redis.port"));
+    Integer db = Integer.parseInt(env.getProperty("spring.redis.db"));
+    conf.setDatabase(db);
+    conf.setHostName(url);
+    conf.setPort(port);
+    return conf;
+  }
+
+
+  @Autowired
+  private Environment env;
+}
